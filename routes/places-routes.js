4dd9f1d2a54f8fdf8a767a167c2931a3a37a4da8
SheriffSoco <japenes@gmail.com>
2022-12-24 15:31:37 -0800

    Add express server with error handling

---
+++ routes/places-routes.js
@@ -0,0 +1,82 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.784474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.784474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.784474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+  {
+    id: "p4",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.784474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u3",
+  },
+];
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  if (places.length === 0) {
+    const error = new Error('Could not find any places for the provided user id.')
+    error.code = 404;
+    next(error); //throw and next work the same (next is preferred)
+  }
+
+  res.json({ places });
+});
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error; //throw and next work the same (next is preferred)
+  }
+
+  res.json({ place });
+});
+
+module.exports = router;
